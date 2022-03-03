795afe929fa205de7dcbf7766544349462894116
Kim-JW <bbam03@naver.com>
2022-03-03 17:46:11 +0900

    module express finish

---
+++ helloweb-ex02/routes/hello.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.route('/01').get(function(req, resp){
+    console.log("!!!");
+    resp.writeHead(200, {
+        'Content-type' : 'text/html'
+    });
+    resp.end("<h1>/hello/01</h1>");  
+});
+
+module.exports = router;
